3e4f2ef37e1a0b83dc5f2025b3df5c5ea7b0219f
alexoff <davydovaleksey932@gmail.com>
2021-07-29 13:14:38 +1000

    Добавлена функция получения истории ставок

--- sender.py
+++ sender.py
@@ -9,7 +9,13 @@ class Sender:
         self.__driver = webdriver.Chrome('chromedriver')
         self.__driver.get("https://www.fonbet.ru/")
         # переходим на страницу авторизации
-        self.__driver.find_element_by_class_name("header-btn").click()
+        element = WebDriverWait(self.__driver, 5).until(
+            ec.presence_of_element_located((
+                By.XPATH,
+                "//a[contains(text(), 'Вход')]"
+            ))
+        )
+        self.__driver.find_element_by_xpath("//a[contains(text(), 'Вход')]").click()
         # получаем поля ввода
         login_in, password_in = self.__driver.find_elements_by_class_name("ui__field")
         login_in.clear()
@@ -53,10 +59,56 @@ class Sender:
                     ))
                 )
                 self.__driver.find_element_by_xpath("//div[contains(text(), 'Кто выиграет иннинг N')]")
+                # т.к. начинать ставить мы будем только с первого иннинга
+                self.__driver.find_element_by_xpath("//div[contains(text(), 'Иннинг 1')]")
                 filtered_events.append(event)
             except Exception:
                 continue
         return filtered_events
+
+    def get_history_bets(self):
+        """
+        Функция для получения истории ставок
+        :return: Список диктов с полями:
+                    - time - Время ставки
+                    - id - Уникальный номер ставки
+                    - sum - Сумма ставкки
+                    - result - Результат ставки (Выигрыш, Проигрыш)
+        """
+        self.__driver.get('https://www.fonbet.ru/account/history/bets')
+        element = WebDriverWait(self.__driver, 5).until(
+            ec.presence_of_element_located((
+                By.CLASS_NAME,
+                "bets-list__data"
+            ))
+        )
+        bets = self.__driver.find_element_by_class_name('bets-list__data')
+        history_bets = bets.find_elements_by_xpath("//div[starts-with(@class, 'operation-row')]")
+        history_results = list()
+        for i, bet in enumerate(history_bets, 1):
+            # если мы находим ячейку с датой не для первого элемента, значит дальше ставки за другой день
+            # и нас они не интересуют
+            # возможно стоит запоминать время первой ставки из таблицы и сохранять,
+            # чтобы при последующем просмотре истории не просматривать старые матчи
+            # цикл в цикле гребанный костыль, по другому пока хз как
+            if i != 1:
+                try:
+                    bet.find_element_by_class_name('column column-1 _hasDate')
+                    break
+                except:
+                    pass
+            time = bet.find_elements_by_xpath("//div[contains(@class, 'column column-2')]")[i].text
+            id_ = bet.find_elements_by_xpath("//div[contains(@class, 'column column-3')]")[i].text
+            sum_ = bet.find_elements_by_xpath("//div[contains(@class, 'column column-5')]")[i].text
+            res = bet.find_elements_by_xpath("//div[contains(@class, 'column column-6')]")[i].text
+            history_results.append({
+                'time': time,
+                'id': id_,
+                'sum': sum_,
+                'result': res
+            })
+
+        return history_results
 
     def close(self):
         self.__driver.close()
